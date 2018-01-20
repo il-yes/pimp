0c6378fcb3eef138ffcaa570220b5ce2cb5035ef
Manuel <manuelvincentsully@gmail.com>
2018-01-20 14:06:20 +0100

    DoctrineMigrationsBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new PrestationBundle\PrestationBundle(),
             new OrderBundle\OrderBundle(),
             new ProductBundle\ProductBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
